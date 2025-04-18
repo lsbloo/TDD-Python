c2fe9dbe5431927835506211f6e383834418ca35
lsbloo <osvaldo.airon@dcx.ufpb.br>
2025-04-17 21:42:09 -0300

    new unit tests

--- initsuperlist/superlist/lists/tests.py
+++ initsuperlist/superlist/lists/tests.py
@@ -1,11 +1,28 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from lists.views import home_page
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+    ## check if new url /home/ is resolved from framework and home_page function views is attached
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/home/')
+        self.assertEqual(found.func, home_page)
+
+    ## should return html content page correct from home_page function view attached
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+        
+
+ 
 
     
